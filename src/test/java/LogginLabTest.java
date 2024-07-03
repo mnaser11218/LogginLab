173c96f64e1db72b8c189faa071ccd9d094ffe7b
Mohammed <mohammed@Zipcoders-MacBook-Pro-7.local>
2024-07-02 20:39:40 -0400

    updated funal

--- src/test/java/LogginLabTest.java
+++ src/test/java/LogginLabTest.java
@@ -39,13 +39,13 @@ public class LogginLabTest {
         LogginLab lab = new LogginLab();
         lab.setThreshold(finalLimit);
 
-        for (Integer i = 1; i <= finalLimit; i++) {
-            if (lab.thresholdExceeds(i)) {
+        for (Integer i = 1; i > finalLimit; i++) {
+            if (lab.thresholdReached(i)) {
                 logger.log(Level.INFO, "Threshold not reached! It is "+i);
-                assertTrue(lab.thresholdExceeds(i));
+                assertTrue(lab.thresholdReached(i));
             } else {
                 logger.log(Level.INFO, "Threshold finally reached!");
-                assertFalse(lab.thresholdExceeds(i));
+                assertFalse(lab.thresholdReached(i));
             }
         }
     }
